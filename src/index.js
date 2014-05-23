057e5362353c1c1df4df5688498129c9a633c740
Rémy Peru <peru.remy@gmail.com>
2014-05-23 22:14:30 +0200

    first example

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 "use strict";
 
-var game = new Phaser.Game(800, 600, Phaser.CANVAS, 'phaser-example', { preload: preload, create: create });
+var game = new Phaser.Game(800, 600, Phaser.AUTO, 'phaser-example', { preload: preload, create: create });
 
 function preload() {
 
@@ -10,14 +10,28 @@ function preload() {
     //  string by which we'll identify the image later in our code.
 
     //  The second parameter is the URL of the image (relative)
-    game.load.image('einstein', 'assets/test-all-the-things.jpg');
+    game.load.image('test', 'assets/test-all-the-things.jpg');
 
 }
 
 function create() {
 
+    game.physics.startSystem(Phaser.Physics.ARCADE);
+
     //  This creates a simple sprite that is using our loaded image and
     //  displays it on-screen
-    game.add.sprite(0, 0, 'einstein');
+    //  and assign it to a variable
+    var image = game.add.sprite(0, 0, 'test');
 
+    game.physics.enable(image, Phaser.Physics.ARCADE);
+
+    //  This gets it moving
+    image.body.velocity.setTo(200,200);
+
+    //  This makes the game world bounce-able
+    image.body.collideWorldBounds = true;
+
+    //  This sets the image bounce energy for the horizontal
+    //  and vertical vectors. "1" is 100% energy return
+    image.body.bounce.set(1);
 }
